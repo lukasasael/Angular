e5a786e7b65f36e24e7129ab99e1113b87e37295
Lukas Asael <lukasasaelbacelar@gmail.com>
2024-07-30 09:38:59 -0300

    lifecyclehooks Implemented

--- AngularIntermediate/src/app/app.component.ts
+++ AngularIntermediate/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from "./demo/demo.component";
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, DemoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AngularIntermediate';
+  inputText: string = '';
+  destroy: boolean = true;
+  OnSubmit(inputEl: HTMLInputElement) {
+    this.inputText = inputEl.value;
+  }
+  destroyComponent() {
+    this.destroy = !this.destroy;
+  }
 }
--- AngularIntermediate/src/app/demo/demo.component.ts
+++ AngularIntermediate/src/app/demo/demo.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
   imports: [],
   templateUrl: './demo.component.html',
-  styleUrl: './demo.component.css'
+  styleUrl: './demo.component.css',
 })
-export class DemoComponent {
+export class DemoComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() value: string = 'procademy';
 
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    console.log('ngOnChanges called');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
 }
